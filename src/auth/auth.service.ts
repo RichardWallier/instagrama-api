c46314d8a9f1fcf28cf37037efc0bab726bb022f
Richard Wallier <ealliero@gmail.com>
2024-04-30 21:30:15 -0300

    feat: add class validator

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthLogic } from './logic/auth.logic';
 import { User } from '@prisma/client';
 
@@ -7,9 +7,6 @@ export class AuthService {
   constructor(private authLogic: AuthLogic) {}
 
   async login(email: string, password: string): Promise<User | null> {
-    if (!email || !password) {
-      throw new BadRequestException('Email and password are required');
-    }
     return this.authLogic.login(email, password);
   }
 }
